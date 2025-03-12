a7b95c79d3b6190e5d710086237fdb71bf3dae40
Muskan-23456789 <muskanmashrak654@gmail.com>
2025-03-12 15:24:19 +0530

    image url

--- src/components/Usercard.jsx
+++ src/components/Usercard.jsx
@@ -2,7 +2,7 @@ const UserCard = ({name, email, age, image}) => {
     return (
         <div className="bg-white rounded-lg shadow-lg p-6 m-4">
             <img 
-                src="https://media.istockphoto.com/id/1480574526/photo/happy-multigenerational-people-having-fun-sitting-on-grass-in-a-public-park.webp?b=1&s=612x612&w=0&k=20&c=pFM42auh4hBX6Srn2JS8xiLOqWxo2nfC5_GSZMAh6y8=" 
+                src={image} 
                 alt={name}
                 className="w-full h-48 object-cover rounded-lg mb-4"
             />
